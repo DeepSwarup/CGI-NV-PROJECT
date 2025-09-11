3a4829896f21be4815a9e7078cf2c11ae652a404
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2025-09-12 01:45:51 +0530

    fixed basic error

--- bankApp/src/main/java/com/bank/bankApp/dtos/TermAccountRequest.java
+++ bankApp/src/main/java/com/bank/bankApp/dtos/TermAccountRequest.java
@@ -1,5 +1,6 @@
 package com.bank.bankApp.dtos;
 
+// Lombok annotations are kept for documentation but we add manual methods
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,4 +15,38 @@ public class TermAccountRequest {
     private int months;
     private double initialDeposit;
     private Long customerId;
+
+    // --- Manual Getters and Setters ---
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public void setMonths(int months) {
+        this.months = months;
+    }
+
+    public double getInitialDeposit() {
+        return initialDeposit;
+    }
+
+    public void setInitialDeposit(double initialDeposit) {
+        this.initialDeposit = initialDeposit;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
 }
