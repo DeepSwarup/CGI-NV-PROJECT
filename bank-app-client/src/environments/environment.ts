6fdeec3970bb5cb84503f7eea993fc04b0076054
Yash Firake <yashfirake2002@gmail.com>
2025-09-13 23:05:06 +0530

    environment routes fixed

--- bank-app-client/src/environments/environment.ts
+++ bank-app-client/src/environments/environment.ts
@@ -1,14 +1,9 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/bank-api',
-  customersUrl: 'http://localhost:8080/bank-api/customers',
-  accountsUrl: 'http://localhost:8080/bank-api/accounts/customer',
-  nomineesUrl: 'http://localhost:8080/bank-api/nominees',
-  beneficiariesUrl: 'http://localhost:8080/bank-api/beneficiaries',
-
   validation: {
-    maxNomineesPerAccount: 4,
-    maxBeneficiariesPerAccount: 10
+    // maxNomineesPerAccount: 4,
+    // maxBeneficiariesPerAccount: 10
   },
   ui: {
     searchDelay: 500
