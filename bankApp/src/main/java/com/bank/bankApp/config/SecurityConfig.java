9efdcb0db08aafe1123adbc4c3d1ccc8ff7e4c94
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2025-09-18 11:46:20 +0530

    original

--- bankApp/src/main/java/com/bank/bankApp/config/SecurityConfig.java
+++ bankApp/src/main/java/com/bank/bankApp/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://cgi-bank-app.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
